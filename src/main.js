d32f09dfda1dc0f746f321d53d446ebb301c7ab6
ratto <rattopedro@gmail.com>
2022-12-02 21:11:11 -0300

    Initial commit; axios and vue bootstrap install

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+// Make Axios accessible so we can consume API
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
